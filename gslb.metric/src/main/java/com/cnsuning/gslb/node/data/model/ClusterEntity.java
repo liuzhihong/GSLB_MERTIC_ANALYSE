76bd7f96cebc8d944dd9a7c630432aa259631b08
liuzhihong <664737215@qq.com>
2015-03-06 15:37:10 +0800

    11111111111111111111

---
+++ gslb.metric/src/main/java/com/cnsuning/gslb/node/data/model/ClusterEntity.java
@@ -0,0 +1,51 @@
+package com.cnsuning.gslb.node.data.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClusterEntity {
+    private List<HostEntity> host = new ArrayList<HostEntity>();
+    private String name;
+    private String localtime;
+    private String owner;
+    private String latlong;
+    private String url;
+
+    public List<HostEntity> getHost() {
+        return host;
+    }
+    public void setHost(List<HostEntity> host) {
+        this.host = host;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocaltime() {
+        return localtime;
+    }
+    public void setLocaltime(String localtime) {
+        this.localtime = localtime;
+    }
+    public String getOwner() {
+        return owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    public String getLatlong() {
+        return latlong;
+    }
+    public void setLatlong(String latlong) {
+        this.latlong = latlong;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
---
+++ gslb.metric/src/main/java/com/cnsuning/gslb/node/data/model/HostEntity.java
@@ -0,0 +1,99 @@
+package com.cnsuning.gslb.node.data.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HostEntity {
+    private List<MetricEntity> metric = new ArrayList<MetricEntity>();
+    private String name;
+    private String ip;
+    private String reported;
+    private String tn;
+    private String tmax;
+    private String dmax;
+    private String location;
+    private String gmond_stated;
+    private String tags;
+    
+    public List<MetricEntity> getMetric() {
+        return metric;
+    }
+    
+    public void setMetric(List<MetricEntity> metric) {
+        this.metric = metric;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getReported() {
+        return reported;
+    }
+
+    public void setReported(String reported) {
+        this.reported = reported;
+    }
+
+    public String getTn() {
+        return tn;
+    }
+
+    public void setTn(String tn) {
+        this.tn = tn;
+    }
+
+    public String getTmax() {
+        return tmax;
+    }
+
+    public void setTmax(String tmax) {
+        this.tmax = tmax;
+    }
+
+    public String getDmax() {
+        return dmax;
+    }
+
+    public void setDmax(String dmax) {
+        this.dmax = dmax;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getGmond_stated() {
+        return gmond_stated;
+    }
+
+    public void setGmond_stated(String gmond_stated) {
+        this.gmond_stated = gmond_stated;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+    
+    
+}
